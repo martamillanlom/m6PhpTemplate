d74d11af247df0fceede8e25ae4be7cd70d4506b
Marta Millan Lombraña <marta.millan@iesjoandaustria.org>
2022-11-02 17:08:28 +0100

    template

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+function get_head(){
+    require "templates/head.php";
+}
+
+function get_header($site){
+    require "templates/header.php";
+}
+
+function get_footer(){
+    require "templates/footer.php";
+}
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require "functions.php";
+
+get_head();
+
+get_header("home"); 
+
+?>
+    
+<h1>hola</h1>
+
+
+<?php get_footer(); ?>
